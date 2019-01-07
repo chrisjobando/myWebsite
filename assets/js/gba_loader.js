f80831013eb6cf81193a1dcde03c9df192acda25
Christopher Obando <chrisjobando@gmail.com>
2019-01-07 15:01:35 -0500

    Trying to make more responsive image on shadowknight page

--- assets/js/gba_loader.js
+++ assets/js/gba_loader.js
@@ -1,22 +1,23 @@
 (function () {
-    const key_filter = ["arrowup", "arrowdown", "arrowleft", "arrowright", "x", "z", "backspace", "enter"];
-    document.addEventListener('DOMContentLoaded', () => {
-        loader = document.querySelector(".gba_loader");
-        loader && loader.addEventListener("click", (e) => {
-            let ninja = document.createElement("iframe");
-            ninja.width = 480;
-            ninja.height = 320;
-            ninja.src = "/assets/gba/index.html";
+  const key_filter = ['arrowup', 'arrowdown', 'arrowleft', 'arrowright', 'x', 'z', 'backspace', 'enter'];
+  document.addEventListener('DOMContentLoaded', () => {
+          loader = document.querySelector('.gba_loader');
+          loader && loader.addEventListener('click', (e) => {
+            let ninja = document.createElement('iframe');
+            ninja.width = 400;
+            ninja.height = 266.67;
+
+            ninja.src = '/assets/gba/index.html';
             e.currentTarget.replaceWith(ninja);
             ninja.focus();
             let key_chomp = (e) => {
                 if (key_filter.includes(e.key.toLowerCase())) {
-                    e.preventDefault();
-                    return false;
+                  e.preventDefault();
+                  return false;
                 }
-            };
-            [ninja, ninja.contentWindow].forEach((x) => x.addEventListener("keydown", key_chomp, false));
-        });
-    });
+              };
 
+            [ninja, ninja.contentWindow].forEach((x) => x.addEventListener('keydown', key_chomp, false));
+          });
+        });
 })();
